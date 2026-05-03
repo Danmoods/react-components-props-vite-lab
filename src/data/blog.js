07b6c6f256226431e38792dfc45620617a16a50d
Danmoods <saitamang1234@gmail.com>
2026-05-03 23:56:42 +0300

    Final changes

--- src/data/blog.js
+++ src/data/blog.js
@@ -8,14 +8,14 @@ const blogData = {
     {
       id: 1,
       title: "Components 101",
-      date: "December 15, 2020",
+      date: "May 3, 2026",
       preview: "Setting up the building blocks of your site",
       minutes: 5,
     },
     {
       id: 2,
       title: "React Data Flow",
-      date: "December 11, 2020",
+      date: "May 3, 2026",
       preview: "Passing props is never passé",
       minutes: 15,
     },
